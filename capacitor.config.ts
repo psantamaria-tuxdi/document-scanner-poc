73bfede2c856ffe7c54ee6addfa1b2c9cb89acd1
PabloSantaMaria <pabloj.santamaria@gmail.com>
2025-09-01 14:52:36 -0300

    feat: update app identifier and add usage descriptions for camera and photo library

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.scanner-test',
   appName: 'scan-poc',
   webDir: 'www'
 };
