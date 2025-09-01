339807886f0368b4c74409ebe69af3a4f203e10f
PabloSantaMaria <pabloj.santamaria@gmail.com>
2025-09-01 10:56:12 -0300

    feat: add photo gallery functionality with camera integration and update dependencies

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/angular/standalone';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonImg,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonIcon,
+    IonFabButton,
+    IonFab,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  ],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
